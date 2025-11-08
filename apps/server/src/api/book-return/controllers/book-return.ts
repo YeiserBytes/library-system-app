b71f091f8114dd4e4b0362d6bf958570e89055f6
YeiserBytes <97372908+YeiserBytes@users.noreply.github.com>
2025-11-08 00:50:29 -0400

    update last changed

---
+++ apps/server/src/api/book-return/controllers/book-return.ts
@@ -0,0 +1,7 @@
+/**
+ * book-return controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::book-return.book-return');
---
+++ apps/server/src/api/book-return/services/book-return.ts
@@ -0,0 +1,7 @@
+/**
+ * book-return service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::book-return.book-return');
